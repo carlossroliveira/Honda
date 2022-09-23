98543c1bb8f981cebf3076e6ac83b8a2e1f991f4
carlos oliveira <carlos.sroliveira@hotmail.com>
2022-09-23 15:59:02 -0300

    Configuração e remoção de pasta

--- src/__tests__/Layout/main.test.tsx
+++ src/__tests__/Layout/main.test.tsx
@@ -2,23 +2,33 @@
 // Packages
 // -------------------------------------------------
 import React from 'react';
-
 import 'jest-styled-components';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { render, screen } from '@testing-library/react';
 // -------------------------------------------------
 // Components
 // -------------------------------------------------
 import { Layout } from '../../components/Layout';
 // -------------------------------------------------
-// Utils
+// Context
 // -------------------------------------------------
-import WithThemeComponent from '../../utils/withThemeComponent';
-
-const LayoutWithTheme = WithThemeComponent(Layout);
+import { ThemeProviderApplication } from '../../Context/contextApplication/ContextTheme';
+// -------------------------------------------------
+// Styles
+// -------------------------------------------------
+import dark from '../../styles/themes/dark';
 
 describe('Layout Testing', () => {
   it('Should...', () => {
-    render(<LayoutWithTheme />);
+    render(
+      <ThemeProvider theme={dark}>
+        <ThemeProviderApplication>
+          <Layout />
+        </ThemeProviderApplication>
+      </ThemeProvider>,
+    );
+
+    screen.logTestingPlaygroundURL();
   });
 });
